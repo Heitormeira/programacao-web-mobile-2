edb65e39ccbb8018408a62b2c2f6ec8751d4b24c
Heitormeira <heitormeira14@gmail.com>
2026-08-27 19:14:39 +0000

    exercicio 263

---
+++ app/exercicio-246/page.js
@@ -0,0 +1,19 @@
+export default function Exercicio246() {
+  return (
+    <main>
+      <p className="paragrafo">
+        Meu nome é Heitor, estudo na UNICAP e sou do Náutico!
+      </p>
+
+      <a href="https://www.nautico-pe.com.br/">
+        Site do Náutico
+      </a>
+
+      <br />
+
+      <a href="https://www.youtube.com/watch?v=zszpONEARjY">
+        Vídeo no YouTube
+      </a>
+    </main>
+  );
+}
---
+++ app/exercicio-263/page.js
@@ -0,0 +1,41 @@
+export default function Exercicio263() {
+  return (
+    <main>
+      <div className="produtos">
+        <div className="produto">
+          <img src="/tenis1.jpg" />
+          <p>Tênis Náutico Vermelho</p>
+          <p>R$ 129,90</p>
+        </div>
+
+        <div className="produto">
+          <img src="/tenis2.jpg" />
+          <p>Tênis Náutico Branco</p>
+          <p>R$ 149,90</p>
+        </div>
+
+        <div className="produto">
+          <img src="/tenis3.jpg" />
+          <p>Tênis Náutico Azul</p>
+          <p>R$ 179,90</p>
+        </div>
+
+        <div className="produto">
+          <img src="/tenis4.jpg" />
+          <p>Sapato Engraçado</p>
+          <p>R$ 670,00</p>
+        </div>
+
+        <div className="produto">
+          <img src="/tenis5.jpg" />
+          <p>Air Jordan 4</p>
+          <p>R$ 420,00</p>
+        </div>
+      </div>
+
+      <footer>
+        Loja do Heitor
+      </footer>
+    </main>
+  );
+}

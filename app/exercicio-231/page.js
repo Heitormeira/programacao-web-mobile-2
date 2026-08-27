35577b0c0a5b3f731c61e526a3d8357df497a43f
Heitormeira <heitormeira14@gmail.com>
2026-08-27 18:34:45 +0000

    exercicio 231

---
+++ app/exercicio-231/page.js
@@ -0,0 +1,13 @@
+export default function Exercicio231() {
+  return (
+    <main>
+      <div className="vermelho">
+        DIV vermelho
+      </div>
+
+      <div className="verde">
+        DIV verde
+      </div>
+    </main>
+  );
+}
